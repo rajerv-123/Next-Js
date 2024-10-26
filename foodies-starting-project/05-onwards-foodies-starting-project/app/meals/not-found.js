36d1f8af65d1723a35eaf94aeaf92a2850171d2c
RajeevPatel <rajeev.patel@areteminds.com>
2024-10-26 19:08:07 +0530

    share meal section added here

---
+++ foodies-starting-project/05-onwards-foodies-starting-project/app/meals/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFoundError() {
+  return (
+    <>
+      <main className="not-found">
+        <h1>Meal not found</h1>
+        <p>Unfortunately, we could not find the requested page or meal data</p>
+      </main>
+    </>
+  );
+}
