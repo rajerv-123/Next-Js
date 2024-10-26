0ea6373b6bf117664a809564d8cac262f06cd07d
RajeevPatel <rajeev.patel@areteminds.com>
2024-10-26 17:36:51 +0530

    error handling is completed

---
+++ foodies-starting-project/05-onwards-foodies-starting-project/app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occurred</h1>
+      <p>Failed to fetch meal data please try agian</p>
+    </main>
+  );
+}
