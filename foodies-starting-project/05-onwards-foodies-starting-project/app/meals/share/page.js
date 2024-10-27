b6fe8cc3fc0addd7fadfb3924ef017eebd4550d8
RajeevPatel <rajeev.patel@areteminds.com>
2024-10-27 12:30:46 +0530

    added async function for adding posting the share data to server locally here

--- foodies-starting-project/05-onwards-foodies-starting-project/app/meals/share/page.js
+++ foodies-starting-project/05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -1,4 +1,6 @@
-import classes from './page.module.css';
+import { shareMeal } from "@/lib/actions";
+import ImagePicker from "../image-picker";
+import classes from "./page.module.css";
 
 export default function ShareMealPage() {
   return (
@@ -10,7 +12,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -32,13 +34,14 @@ export default function ShareMealPage() {
           <p>
             <label htmlFor="instructions">Instructions</label>
             <textarea
+              className={classes.input}
               id="instructions"
               name="instructions"
               rows="10"
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
